e955c4fc6b38847fbd1a6635dd067e09f1bec7d8
JFleming4 <justinfleming95@gmail.com>
2016-04-02 20:37:03 -0400

    Added moves to board - Bronwyn and Justin

--- src/Board.java
+++ src/Board.java
@@ -3,16 +3,70 @@ public class Board {
 	
 	private Person leftPerson;
 	private Person rightPerson;
-	private int leftPosition;
-	private int rightPosition;
-	
+	private Person[] board;
 
 
 	public int getLeftPosition(){
-		return leftPosition;
+		for(int i = 0; i < board.length; i++){
+			if(board[i] == leftPerson){
+				return i;
+			}
+		}
+		return -1;
 	}
 	
 	public int getRightPosition(){
-		return rightPosition;
+		for(int i = 0; i < board.length; i++){
+			if(board[i] == rightPerson){
+				return i;
+			}
+		}
+		return -1;
 	}
+	
+	public int getPosition(Person person){
+		for(int i = 0; i < board.length; i++){
+			if(board[i] == person){
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public boolean canMoveRight(Person person){
+		if(board.length -1 == getPosition(person)){
+			return false;
+		}
+		if(board[getPosition(person)+1]!=null){
+			return false;
+		}
+		return true;
+	}
+	public boolean canMoveLeft(Person person){
+		if(0 == getPosition(person)){
+			return false;
+		}
+		if(board[getPosition(person)-1]!=null){
+			return false;
+		}
+		return true;
+	}
+	
+	/**
+	 * Must check valid move first
+	 * @param person
+	 */
+	public void movePersonLeft(Person person){
+		board[getPosition(person) -1] = person;
+		board[getPosition(person)] = null;
+	}
+	/**
+	 * Must check valid move first
+	 * @param person
+	 */
+	public void movePersonRight(Person person){
+		board[getPosition(person) +1] = person;
+		board[getPosition(person)] = null;
+	}
+	
 }
